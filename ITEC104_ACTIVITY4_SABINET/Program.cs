3e89d8a8a3c30c13068de78f1b5c111f3e811619
John Philip Sabinet <93175281+TeachDian@users.noreply.github.com>
2022-09-21 13:02:02 +0800

    Add files via upload

---
+++ ITEC104_ACTIVITY4_SABINET/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Title = "Activity 4";
+Console.ForegroundColor = ConsoleColor.Green;
+
+
+const int n = 5;
+int[] numbers = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Enter Element : ", i + 1);
+    string str = Console.ReadLine();
+    numbers[i] = int.Parse(str);
+
+}
+
+Console.Write("\nODD NUMBERS : ");
+for (int i = 0; i < n; i++)
+{
+    if (numbers[i] % 2 == 1)
+        Console.Write("{0} ", numbers[i]);
+}
+
+
+Console.Write("\nEVEN NUMBERS : ");
+for (int i = 0; i < n; i++)
+{
+    if (numbers[i] % 2 == 0)
+        Console.Write("{0} ", numbers[i]);
+}
+Console.Read();
