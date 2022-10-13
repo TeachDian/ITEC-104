b262971c30a0cf4f7755332c9486d97eed72ad2b
John Philip Sabinet <93175281+TeachDian@users.noreply.github.com>
2022-10-13 11:57:37 +0800

    Update Program.cs

--- ITEC104_ACTIVITY5_SABINET/Program.cs
+++ ITEC104_ACTIVITY5_SABINET/Program.cs
@@ -1,31 +1,3 @@
-﻿// internal class Program
-// {
-//     private static void Main(string[] args)
-//     {
-//         IList<int> list = new List<int>(){
-//             10, 20, 35 , 65, 53, 48 , 5, 1
-//         };
-
-//         foreach (var item in list)
-//         {
-//             for (int i = 0; i < 5; i++)
-//             {
-//                 Console.WriteLine(item);
-//                 Console.ReadKey();
-//             }
-
-//         }
-
-
-//     }
-//     public static List<int> test(List<int> nums)
-//     {
-//         return nums.Where(n => n % 10 < 5).ToList();
-//         //Console.WriteLine(test);
-//     }
-
-// }
-
 internal class Calculator
 {
     static void Main(string[] args)
