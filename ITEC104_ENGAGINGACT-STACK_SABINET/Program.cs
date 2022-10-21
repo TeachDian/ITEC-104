5a0e599d9a1dd88c9348a41a9cc3f8ddee3c44e8
Philip Gardiola Sabinet <93175281+TeachDian@users.noreply.github.com>
2022-10-22 07:16:12 +0800

    Add files via upload

---
+++ ITEC104_ENGAGINGACT-STACK_SABINET/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+Stack st = new Stack();
+st.Push('A');
+st.Push('M');
+st.Push('G');
+st.Push('W');
+
+Console.WriteLine("Current Stack: ");
+foreach (char c in st)
+{
+    Console.Write(c + " ");
+}
+Console.WriteLine();
+st.Push('H');
+st.Push('V');
+Console.WriteLine("The next poppable value in stack: {0} ", st.Peek());
+Console.WriteLine("urrent stack: ");
+foreach (char c in st)
+{
+    Console.Write(c + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Removing values ");
+st.Pop();
+st.Pop();
+st.Pop();
+Console.WriteLine("Current stack: ");
+foreach (char c in st)
+{
+    Console.Write(c + " ");
+}
+Console.ReadKey(true);
